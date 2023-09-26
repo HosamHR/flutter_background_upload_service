4efb5725209ddca17528ceb5f41e56804bd6a52b
HosamHR <hosam@neureka.ai>
2023-09-26 18:17:30 +0300

    fix import

--- packages/flutter_background_upload_service/example/lib/main.dart
+++ packages/flutter_background_upload_service/example/lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:ui';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_upload_service/flutter_background_upload_service.dart';
-import 'package:flutter_background_upload_service_android/flutter_background_service_android.dart';
+import 'package:flutter_background_upload_service_android/flutter_background_upload_service_android.dart';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
